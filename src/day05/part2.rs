ca23d9ff7d2cc1b81839b6197e8ae2550a9efc8d
Joep Eijkels <j.eijkels@emendis.nl>
2023-12-04 20:34:27 +0100

    Add Day 4 and 5 boilerplating

---
+++ src/day05/part2.rs
@@ -0,0 +1,14 @@
+#![allow(clippy::needless_return)]
+
+use std::fs;
+
+pub fn main() {
+    let input: String = fs::read_to_string("./inputs/day05/input.txt")
+        .expect("Was not able to read, does the file exist?");
+
+    println!("{}", part2(&input))
+}
+
+fn part2(input: &str) -> String {
+    todo!()
+}
