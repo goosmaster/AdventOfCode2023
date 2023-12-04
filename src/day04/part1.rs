3c0bd0fe2d4ebbec0932294f6ec6349b3cd50e2e
Joep Eijkels <j.eijkels@emendis.nl>
2023-12-04 21:19:20 +0100

    Solve day 4 part 1

--- src/day04/part1.rs
+++ src/day04/part1.rs
@@ -2,6 +2,13 @@
 
 use std::fs;
 
+#[derive(Debug, PartialEq)]
+struct Card {
+    card_id: u8,
+    winning_numbers: Vec<u8>,
+    card_numbers: Vec<u8>
+}
+
 pub fn main() {
     let input: String = fs::read_to_string("./inputs/day04/input.txt")
         .expect("Was not able to read, does the file exist?");
@@ -10,5 +17,98 @@ pub fn main() {
 }
 
 fn part1(input: &str) -> String {
-    todo!()
+    let cards = get_cards(input);
+    let mut total: usize = 0;
+
+    for card in cards {
+        total += get_card_score(card);
+    }
+
+    return total.to_string();
+}
+
+fn get_cards(input: &str) -> Vec<Card> {
+    let mut cards: Vec<Card> = Vec::new();
+
+    for card in input.lines() {
+        let parts = card.split('|').collect::<Vec<&str>>();
+        let card_numbers = parse_card_numbers(parts.clone());
+        let card_info = parts.first().unwrap().split(':').collect::<Vec<&str>>();
+        let winning_numbers = parse_card_numbers(card_info.clone());
+        let card_id = card_info.first().unwrap().split("Card").last().unwrap().trim().parse::<u8>().unwrap();
+
+        cards.push(Card {
+            card_id,
+            winning_numbers,
+            card_numbers,
+        })
+    }
+
+    return cards;
+}
+
+fn parse_card_numbers(parts: Vec<&str>) -> Vec<u8> {
+    return parts.last().unwrap().trim().split(' ')
+        .filter(|s: &&str| !s.is_empty()).collect::<Vec<&str>>()
+        .iter().map(|s: &&str| s.parse::<u8>().unwrap()).collect::<Vec<u8>>();
+}
+
+fn get_card_score(card: Card) -> usize {
+    let mut score: usize = 0;
+
+    for winning_number in card.winning_numbers {
+        if card.card_numbers.contains(&winning_number) {
+            if score == 0 {
+                score = 1;
+            } else {
+                score *= 2;
+            }
+        }
+    }
+
+    return score;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_calculates_the_points_of_the_given_scratchcard() {
+        let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+        let output = part1(input);
+
+        assert_eq!("13", output);
+    }
+
+    #[test]
+    fn it_gets_the_correct_card_info_from_string_input() {
+        let input = "Card   1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
+        let output = get_cards(input);
+
+        assert_eq!(vec![
+            Card{
+                card_id: 1,
+                winning_numbers: vec![41, 48, 83, 86, 17],
+                card_numbers: vec![83, 86, 6, 31, 17, 9, 48, 53],
+            },
+        ], output);
+    }
+
+    #[test]
+    fn it_counts_the_score_for_the_given_card() {
+        let input = Card{
+            card_id: 1,
+            winning_numbers: vec![41, 48, 83, 86, 17],
+            card_numbers: vec![83, 86, 6, 31, 17, 9, 48, 53],
+        };
+        let output = get_card_score(input);
+
+        assert_eq!(8, output);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod common;
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
 fn main() {
     println!("[day]-[part] :");
@@ -20,6 +21,8 @@ fn main() {
         "2-2" => day02::part2::main(),
         "3-1" => day03::part1::main(),
         "3-2" => day03::part2::main(),
+        "4-1" => day04::part1::main(),
+        "4-2" => day04::part2::main(),
         _ => println!("Invalid day-part combination")
     }
 }
