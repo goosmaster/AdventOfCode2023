74b89b27e71fa3fa39303f5765ad332dd78f9c89
Joep Eijkels <j.eijkels@emendis.nl>
2023-12-12 12:25:52 +0100

    Refactor interaction & calling of the day-part

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate core;
 
 use std::fs;
 use std::io::{Error, stdin};
+use std::process::exit;
 use advent_of_code_2023::common::Types::part::Part;
 
 mod common;
@@ -16,7 +17,6 @@ use Applications::SeedFertilizer;
 use Applications::WaitForIt;
 use Applications::CamelCards;
 use Applications::HauntedWasteland;
-use common::Types::part;
 
 fn main() {
     // if running in --interactive
@@ -34,27 +34,53 @@ fn interactive() {
     };
 
     println!("[day]-[part]:");
+    selection();
+}
+
+fn selection() {
     let mut input = String::new();
     stdin().read_line(&mut input).expect("Failed to read line");
 
-    match input.trim() {
-        "1-1" => Trebuchet::main::part(Part::from(1)),
-        "1-2" => Trebuchet::main::part(Part::from(2)),
-        "2-1" => CubeConundrum::main::part(Part::from(1)),
-        "2-2" => CubeConundrum::main::part(Part::from(2)),
-        "3-1" => GearRatios::main::part(Part::from(1)),
-        "3-2" => GearRatios::main::part(Part::from(2)),
-        "4-1" => Scratchcards::main::part(Part::from(1)),
-        "4-2" => Scratchcards::main::part(Part::from(2)),
+    if input.trim().to_lowercase() == "exit" {
+        exit(0);
+    }
 
-        "5-1" => SeedFertilizer::src::main::part(Part::from(1)),
-        "5-2" => SeedFertilizer::src::main::part(Part::from(2)),
-        "6-1" => WaitForIt::src::main::part(Part::from(1)),
-        "6-2" => WaitForIt::src::main::part(Part::from(2)),
-        "7-1" => CamelCards::src::main::part(Part::from(1)),
-        "7-2" => CamelCards::src::main::part(Part::from(2)),
-        "8-1" => HauntedWasteland::src::main::part(Part::from(1)),
-        "8-2" => HauntedWasteland::src::main::part(Part::from(2)),
+    // let id = game_metadata.split(' ').collect::<Vec<&str>>().clone()
+    //         .iter().next_back().unwrap().parse::<u32>().unwrap();
+
+    let mut parts_iter = input.trim().split('-').map(|s| s.parse::<u8>().unwrap());
+    let day = parts_iter.next().unwrap();
+    let part = parts_iter.next().unwrap();
+
+    match day {
+        1 => Trebuchet::main::part(Part::from(part)),
+        2 => CubeConundrum::main::part(Part::from(part)),
+        3 => GearRatios::main::part(Part::from(part)),
+        4 => Scratchcards::main::part(Part::from(part)),
+        5 => SeedFertilizer::src::main::part(Part::from(part)),
+        6 => WaitForIt::src::main::part(Part::from(part)),
+        7 => CamelCards::src::main::part(Part::from(part)),
+        8 => HauntedWasteland::src::main::part(Part::from(part)),
+        9 => todo!(),
+        10 => todo!(),
+        11 => todo!(),
+        12 => todo!(),
+        13 => todo!(),
+        14 => todo!(),
+        15 => todo!(),
+        16 => todo!(),
+        17 => todo!(),
+        18 => todo!(),
+        19 => todo!(),
+        20 => todo!(),
+        21 => todo!(),
+        22 => todo!(),
+        23 => todo!(),
+        24 => todo!(),
+        25 => todo!(),
         _ => println!("Invalid day-part combination")
     }
+
+    println!("Type \"exit\" to quit or another [day]-[part]:");
+    selection();
 }
